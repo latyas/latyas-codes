7220b8d9e002ec0af77500d8a215e79c1de7df1c
Latyas <latyas@gmail.com>
2013-10-19 17:35:04 +0800

    dict.cn webster etymology

---
+++ etym.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import urllib
+import sys
+from bs4 import BeautifulSoup
+import re 
+
+def etymology(key):
+	page = urllib.urlopen("http://www.etymonline.com/index.php?term=" + key).read().decode("utf-8")
+	if 'No matching terms found.' in page:
+		return None
+	try:
+		soup = BeautifulSoup(page)
+		foo = soup.findAll('div', id='dictionary')
+		rep = re.compile('<img.*"/>?')
+		ret = rep.sub('',str(foo[0]))
+		ret = ret.replace('</div>','')
+		ret = ret.replace('<div id="dictionary">','')
+		
+		return ret
+	except:
+		pass
+	return None
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print 'Usage: %s [WORD]' % sys.argv[0]
+		sys.exit(1)
+	foo = etymology(sys.argv[1])
+	if foo:
+		foo = re.sub('<a .*?>.*?</a>','',foo)
+		foo = re.sub('<(dt|dl|dd|span).*?>','',foo)
+		foo = re.sub('</(dt|dl|dd|span)>','',foo)
+
+		foo = foo.replace('\n\n','')
+		foo += '\n'
+		print foo
+	else:
+		print 'Not Found'
---
+++ gre.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import urllib
+import re
+import sys
+from bs4 import BeautifulSoup
+
+def get_context(key):
+    ret = urllib.urlopen('http://www.merriam-webster.com/dictionary/%s' % key).read().decode("utf-8")
+    soup = BeautifulSoup(ret)
+    foo = soup.findAll(attrs={"class":"scnt"})
+    return str(foo)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage: %s [WORD]' % sys.argv[0]
+        sys.exit(1)
+
+    keyword = sys.argv[1]
+    data = get_context(keyword)
+    bs = BeautifulSoup(data)
+    foo = bs.findAll(attrs={'class':'scnt'})
+    for j,i in enumerate(foo):
+        i = str(i)
+        i = re.subn('<div.*?>','',i)[0]
+        i = re.subn('<span.*?>','',i)[0]
+        i = re.subn('<strong.*?>','',i)[0]
+        i = i.replace(r'&lt;','')
+        i = i.replace(r'&gt','')
+        i = i.replace(r'</strong>','')
+        i = i.replace(r'</span>','')
+        i = i.replace(r'</em>','')
+        i = i.replace(r'</div>','\n ')
+        i = re.subn('<a.*?>','',i)[0]
+        i = re.subn('<em.*?>','',i)[0]
+        i = i.replace('</a>','')
+        i = i.replace('Â','')
+        i = ' ' + i
+        i = i.replace(r'  ','')
+        i = i.replace(r'  ','')
+        print '[%d] %s' % (j+1,i)
