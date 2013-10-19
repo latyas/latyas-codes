72fba3e0837088e320a3d7d1e27bd337321c0b2b
Latyas <latyas@gmail.com>
2013-10-19 19:31:49 +0800

    add log function.

--- dict.py
+++ dict.py
@@ -5,6 +5,8 @@ import urllib2
 import sys
 import re
 import bs4
+import os
+import time
 
 def main():
     if len(sys.argv) == 2:
@@ -16,20 +18,34 @@ def main():
             else:
                 isen = False
             xmls = urllib2.urlopen('http://dict.cn/' + word).read()
-
+            trans = ''
             if isen == True:
                 rem = re.compile(r'<li><span>(?P<test>.*?)</strong></li>')
                 for i in rem.findall(xmls):
                     print i.replace('</span><strong>','')
+                    trans += i.replace('</span><strong>','') + ' | '
             else:
                 bs = bs4.BeautifulSoup(xmls)
                 foo = str(bs.findAll(attrs={'class':'layout cn'})[0])
                 rem = re.compile(r'<li><a href=".*?">(?P<test>.*?)</a></li>')
                 for j,i in enumerate(rem.findall(foo)):
                     print j+1,i
+                    trans += i + ' | '
         except:
             print 'Not found.'
+            return
+        #log
 
+        date = '%d-%d-%d' % (time.localtime().tm_year,time.localtime().tm_mon,time.localtime().tm_mday)
+	try:
+        	f = open(os.path.expanduser("~/") + 'dict.log','r+')
+	except:
+        	open(os.path.expanduser("~/") + 'dict.log','w').close()
+        	f = open(os.path.expanduser("~/") + 'dict.log','r+')
+        if not date in f.read():
+            f.writelines(date + '\n')
+        f.writelines('[%s] %s\n' % (word,trans))
+        f.close()
     else:
         help()
 
--- etym.py
+++ etym.py
@@ -2,39 +2,51 @@
 #coding=utf-8
 
 import urllib2
-import sys
+import sys,os
 from bs4 import BeautifulSoup
 import re 
-
+import time
 def etymology(key):
-	page = urllib2.urlopen("http://www.etymonline.com/index.php?term=" + key).read().decode("utf-8")
-	if 'No matching terms found.' in page:
-		return None
-	try:
-		soup = BeautifulSoup(page)
-		foo = soup.findAll('div', id='dictionary')
-		rep = re.compile('<img.*"/>?')
-		ret = rep.sub('',str(foo[0]))
-		ret = ret.replace('</div>','')
-		ret = ret.replace('<div id="dictionary">','')
-		
-		return ret
-	except:
-		pass
-	return None
+    page = urllib2.urlopen("http://www.etymonline.com/index.php?term=" + key).read().decode("utf-8")
+    if 'No matching terms found.' in page:
+        return None
+    try:
+        soup = BeautifulSoup(page)
+        foo = soup.findAll('div', id='dictionary')
+        rep = re.compile('<img.*"/>?')
+        ret = rep.sub('',str(foo[0]))
+        ret = ret.replace('</div>','')
+        ret = ret.replace('<div id="dictionary">','')
+        
+        return ret
+    except:
+        pass
+    return None
 
 if __name__ == '__main__':
-	if len(sys.argv) != 2:
-		print 'Usage: %s [WORD]' % sys.argv[0]
-		sys.exit(1)
-	foo = etymology(sys.argv[1])
-	if foo:
-		foo = re.sub('<a .*?>.*?</a>','',foo)
-		foo = re.sub('<(dt|dl|dd|span).*?>','',foo)
-		foo = re.sub('</(dt|dl|dd|span)>','',foo)
+    if len(sys.argv) != 2:
+        print 'Usage: %s [WORD]' % sys.argv[0]
+        sys.exit(1)
+    foo = etymology(sys.argv[1])
+    if foo:
+        foo = re.sub('<a .*?>.*?</a>','',foo)
+        foo = re.sub('<(dt|dl|dd|span|br).*?>','',foo)
+        foo = re.sub('</(dt|dl|dd|span)>','',foo)
 
-		foo = foo.replace('\n\n','')
-		foo += '\n'
-		print foo
-	else:
-		print 'Not Found'
+        foo = foo.replace('\n\n','')
+        foo += '\n'
+        print foo
+        #log
+
+        date = '%d-%d-%d' % (time.localtime().tm_year,time.localtime().tm_mon,time.localtime().tm_mday)
+        try:
+        	f = open(os.path.expanduser("~/") + 'etym.log','r+')
+        except:
+        	open(os.path.expanduser("~/") + 'etym.log','w').close()
+        	f = open(os.path.expanduser("~/") + 'etym.log','r+')
+        if not date in f.read():
+            f.writelines(date + '\n')
+        f.writelines('[%s]\n%s\n\n\n' % (sys.argv[1],foo))
+        f.close()
+    else:
+        print 'Not Found'
