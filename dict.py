0021d284454cb263b6efcdf64d1edee5ce656e85
latyas <latyas@Induction.(none)>
2013-10-19 11:46:39 +0800

    dict.py inquery word from dict.cn (support chinese and english)

---
+++ dict.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import urllib
+import sys
+import re
+import bs4
+
+def main():
+    if len(sys.argv) == 2:
+        word = sys.argv[1]
+        # if non en?
+        try:
+            if re.findall('\w*',word)[0] == word:
+                isen = True
+            else:
+                isen = False
+            xmls = urllib.urlopen('http://dict.cn/' + urllib.quote(word)).read()
+
+            if isen == True:
+                rem = re.compile(r'<li><span>(?P<test>.*?)</strong></li>')
+                for i in rem.findall(xmls):
+                    print i.replace('</span><strong>','')
+            else:
+                bs = bs4.BeautifulSoup(xmls)
+                foo = str(bs.findAll(attrs={'class':'layout cn'})[0])
+                rem = re.compile(r'<li><a href=".*?">(?P<test>.*?)</a></li>')
+                for j,i in enumerate(rem.findall(foo)):
+                    print j+1,i
+        except:
+            print 'Not found.'
+
+    else:
+        help()
+
+def help():
+    print 'usage:%s [word]' % sys.argv[0]
+
+if __name__ == '__main__':
+    main()
+
