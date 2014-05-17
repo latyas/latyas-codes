f158ab339ef6ac233db4fbcaaa1aa69c4204a0f3
Yangjie Li <latyas@gmail.com>
2014-05-17 15:05:32 +0800

    尝试性的加入自动提取词根

--- etym.py
+++ etym.py
@@ -7,9 +7,32 @@ from bs4 import BeautifulSoup
 import re
 import time
 
+def match(s):
+	r = re.compile('from ([\w\s-]+?)(\([\w\s-]+\))*? "(.*?)"',re.S | re.M)
+	r2 = re.compile('\+ (\w+) "(.*?)"',re.S | re.M)
+
+	l = []
+
+	ret = r.findall(s)
+	for word, foo, mean in ret:
+		if word.count('from') > 1:
+			word = word.split('from')[-1]
+		word = word.strip().split(' ')[-1]
+		l.append((word, mean))
+
+	ret = r2.findall(s)
+	for word, mean in ret:
+		l.append((word, mean))
+	
+	with open('/home/latyas/test_etym.log','a') as f:
+		for i in l:
+			ss = '%s || %s\n' % i
+			f.write(ss)
+			print ss
+
 def process_text(text):
     r = re.compile('<a.*?>(.*?)</a>')
-    return r.sub("\\x1b[31;43;1m\\1\\x1b[0m",text)
+    return r.sub("\\x1b[32;40;1m\\1\\x1b[0m",text)
 
 def etymology(key):
     page = urllib2.urlopen("http://www.etymonline.com/index.php?term=" + key).read().decode("utf-8")
@@ -19,6 +42,7 @@ def etymology(key):
     foo = soup.findAll('div', id='dictionary')
     foo = foo[0].findAll('dd')
     foo = '\n'.join([''.join(map(str,bar.contents)) for bar in foo])
+    match(BeautifulSoup(foo).text)
     foo = process_text(foo)
     foo = BeautifulSoup(foo).text
     foo = foo.replace(r'\x1b','\x1b')
